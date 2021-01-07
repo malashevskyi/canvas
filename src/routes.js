cc01f539c360e465c9489e203fe69537d239abf6
malashevskyi <shevsky1488@gmail.com>
2021-01-07 09:39:02 +0200

    add main route

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,20 @@ import { Switch, Route } from 'react-router-dom';
 
 const Routes = ({ allCanvasNames, canvas }) => {
   const canvasRef = useRef();
+  const MainCanvas = canvas['Necklace'];
   
   return (
     <Fragment>
       <canvas ref={canvasRef}></canvas>
       <Switch>
+        <Route path="/" exact>
+          <MainCanvas ref={canvasRef} />
+        </Route>
         {allCanvasNames.map((name) => {
           let Canvas = canvas[name];
 
           return (
             <Route key={name} path={`/${name}`} exact>
-              {/* <Canvas /> */}
               <Canvas ref={canvasRef} />
             </Route>
           );
