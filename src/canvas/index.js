bd0c1fb0b0fac4ec6d79f5d61a8ee08c99649864
malashevskyi <shevsky1488@gmail.com>
2021-01-13 12:02:45 +0200

    fix name

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,5 +1,5 @@
 export { default as CircleOrnament } from './circleOrnament';
-export { default as Bouquet } from './bouguet';
+export { default as Bouquet } from './bouquet';
 export { default as Jelly_2 } from './jelly_2';
 export { default as Festoon_2 } from './festoon_2';
 export { default as Festoon } from './festoon';
