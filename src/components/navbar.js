94be42b9f5a50c6388a6d77e6e8d006b1da2f712
malashevskyi <shevsky1488@gmail.com>
2021-01-16 13:06:37 +0200

    separate post dates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,26 +6,7 @@ import Preview from './preview';
 import Search from './search';
 import { MenuIsOpenContext } from '../context/menuIsOpenContext';
 import * as previews from '../images/loadImages';
-
-const postDates = {
-  ParticlesSun: '2021-01-16',
-  SwirlOfSquares: '2021-01-15',
-  Orbit_2: '2021-01-14',
-  Orbit: '2021-01-14',
-  CircleOrnament: '2021-01-13',
-  Bouquet: '2021-01-12',
-  Jelly_2: '2021-01-11',
-  Festoon_2: '2021-01-10',
-  Festoon: '2021-01-10',
-  ParticlesInMouseDirection: '2021-01-09',
-  Jelly: '2021-01-08',
-  Necklace: '2021-01-07',
-  StainedGlass: '2021-01-06',
-  StarrySky: '2021-01-05',
-  Balls: '2021-01-04',
-  ParticlesMoveInACircle: '2021-01-03',
-  Firework: '2021-01-02',
-};
+import postDates from '../data/postDates';
 
 const Navbar = ({ onEnteredFilter, filteredCanvasNames }) => {
   const location = useLocation();
---
+++ src/data/postDates.js
@@ -0,0 +1,21 @@
+const postDates = {
+  ParticlesSun: '2021-01-16',
+  SwirlOfSquares: '2021-01-15',
+  Orbit_2: '2021-01-14',
+  Orbit: '2021-01-14',
+  CircleOrnament: '2021-01-13',
+  Bouquet: '2021-01-12',
+  Jelly_2: '2021-01-11',
+  Festoon_2: '2021-01-10',
+  Festoon: '2021-01-10',
+  ParticlesInMouseDirection: '2021-01-09',
+  Jelly: '2021-01-08',
+  Necklace: '2021-01-07',
+  StainedGlass: '2021-01-06',
+  StarrySky: '2021-01-05',
+  Balls: '2021-01-04',
+  ParticlesMoveInACircle: '2021-01-03',
+  Firework: '2021-01-02',
+};
+
+export default postDates;
