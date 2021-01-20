d9e6143d42053b614624f05f09ff329ae2896d54
malashevskyi <shevsky1488@gmail.com>
2021-01-20 03:22:53 +0200

    add react-virtualized

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import classNames from 'classnames';
 import { useLocation, Link } from 'react-router-dom';
+import { List, AutoSizer } from 'react-virtualized';
 
 import Preview from './preview';
 import Search from './search';
@@ -8,9 +9,35 @@ import { MenuIsOpenContext } from '../context/menuIsOpenContext';
 import * as previews from '../images/loadImages';
 import postDates from '../data/postDates';
 
-const Navbar = ({ onEnteredFilter, filteredCanvasNames }) => {
+const Navbar = ({
+  onEnteredFilter,
+  filteredCanvasNames,
+}) => {
   const location = useLocation();
   const [isOpen] = useContext(MenuIsOpenContext);
+
+  function rowRenderer({ key, index, style }) {
+    const name = filteredCanvasNames[index];
+    return (
+      <div
+        style={style}
+        key={key}
+        className={classNames({
+          'navbar-menu--item': true,
+          active: location.pathname === `/${name}`,
+        })}
+        // onClick={() => setIsOpen(false)}
+      >
+        <Link to={name}>
+          <h2 className="navbar-menu--title">{name}</h2>
+          <Preview name={name} preview={previews[name]} />
+          <time dateTime={postDates[name]}>
+            <span>{postDates[name]}</span>
+          </time>
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div
@@ -22,28 +49,22 @@ const Navbar = ({ onEnteredFilter, filteredCanvasNames }) => {
       <div className="navbar--inner">
         <Search onEnteredFilter={onEnteredFilter} />
         <nav className="navbar-menu">
-          <ul className="navbar-menu--list">
-            {filteredCanvasNames.map((name) => {
-              return (
-                <li
-                  className={classNames({
-                    'navbar-menu--item': true,
-                    active: location.pathname === `/${name}`,
-                  })}
-                  key={name}
-                  // onClick={() => setIsOpen(false)}
-                >
-                  <Link to={name}>
-                    <h2 className="navbar-menu--title">{name}</h2>
-                    <Preview name={name} preview={previews[name]} />
-                    <time dateTime={postDates[name]}>
-                      <span>{postDates[name]}</span>
-                    </time>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+          <div className="navbar-menu--list">
+            <AutoSizer>
+              {({ height }) => {
+                return (
+                  <List
+                    width={300}
+                    height={height}
+                    rowCount={filteredCanvasNames.length}
+                    rowHeight={125}
+                    rowRenderer={rowRenderer}
+                    overscanRowCount={3}
+                  />
+                );
+              }}
+            </AutoSizer>
+          </div>
         </nav>
       </div>
     </div>
