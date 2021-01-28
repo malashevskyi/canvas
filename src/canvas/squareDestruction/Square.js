323201d236fc2270f8f6e1ee9246d3bc67572768
malashevskyi <shevsky1488@gmail.com>
2021-01-28 14:02:24 +0200

    update SquareDestruction

--- src/canvas/squareDestruction/Square.js
+++ src/canvas/squareDestruction/Square.js
@@ -1,35 +1,91 @@
 import random from 'canvas-sketch-util/random';
-import { TweenMax, Power4 } from 'gsap';
 
+// commit 12cdbc1 SquareDestruction with TweenMax
 class Square {
-  constructor(context, maxSide, [x, y], color) {
+  constructor(context, maxSide, [x, y]) {
     this.context = context;
     this.angle = random.range(0, Math.PI * 2);
-    this.x = [Math.cos(this.angle) * maxSide / 3  + 100];
-    this.y = [Math.sin(this.angle) * maxSide / 3  + 100];
-    this.fx = x;
-    this.fy = y + 20;
+    this.fx = Math.cos(this.angle) * maxSide / 3  + 40;
+    this.fy = Math.sin(this.angle) * maxSide / 3  + 40;
+    this.x = x;
+    this.y = y + 20;
+    this.xBack = this.x;
+    this.yBack = this.y;
     this.random = Math.random() * 2 + 0.5;
-    this.tlx = TweenMax.from(this.x, { delay: this.random, ease: Power4.easeIn, yoyo: true, repeat: -1, repeatDelay: 0, duration: 7, 0: this.fx })
-    this.tly = TweenMax.from(this.y, { delay: this.random, ease: Power4.easeIn, yoyo: true, repeat: -1, repeatDelay: 0, duration: 7, 0: this.fy })
-    this.tick = 50;
-    // this.color = color;
+    this.colorTick = 100;
+    this.distTickInitial = 200 + Math.trunc(1000 * Math.random());
+    this.distTick = 200 + Math.trunc(1000 * Math.random());
+    this.initTimeout = 100;
+    this.back = true;
   }
   
   render() {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      this.tlx.pause();
-      this.tly.pause();
-    }, 25);
-    this.tick += 0.2;
-    this.color = `hsl(${this.tick}, 50%, 50%)`;
+    // delay in 100 frames
+    if (this.initTimeout) {
+      this.initTimeout--;
+      this.draw();
+      return;
+    }
+    const dCos = (dist, angle) => Math.cos(angle) * dist / this.distTick;
+    const dSin = (dist, angle) => Math.sin(angle) * dist / this.distTick;
+
+    let dx, dy, dist, part;
+
+    // from center
+    if (this.back) {
+      dx = this.fx - this.x;
+      dy = this.fy - this.y;
+      dist = Math.sqrt(dx * dx + dy * dy);
+      part = dist / this.distTick;
+      
+      if (dist >= part) {
+        const angle = Math.atan2(dy, dx);
+        this.x += dCos(dist, angle);
+        this.y += dSin(dist, angle);
+        this.distTick--;
+      } else if (dist < part) {
+        this.x = this.fx;
+        this.y = this.fy;
+      } else if (dist === 0) {
+        this.distTick = this.distTickInitial;
+        this.back = false;
+      }
+    }
+
+    // to center
+    if (!this.back) {
+      dx = this.x - this.xBack;
+      dy = this.y - this.yBack;
+      dist = Math.sqrt(dx * dx + dy * dy);
+      part = dist / this.distTick;
+      
+      if (dist >= part) {
+        const angle = Math.atan2(dy, dx);
+        this.x -= dCos(dist, angle);
+        this.y -= dSin(dist, angle);
+        this.distTick--;
+      } else if (dist < part) {
+        this.x = this.xBack;
+        this.y = this.yBack;
+      } else if (dist === 0) {
+        this.back = true;
+        this.distTick = this.distTickInitial;
+      }
+    }
+
+    this.draw();
+  }
+
+  draw() {
+    this.colorTick += 0.2;
+    this.color = `hsl(${this.colorTick}, 50%, 50%)`;
     this.context.beginPath();
     this.context.fillStyle = this.color;
-    this.context.rect(this.x[0], this.y[0], 10, 10);
+    this.context.rect(this.x, this.y, 1.5, 1.5);
+    this.context.strokeWidth = 0;
     this.context.strokeStyle = this.color;
-    this.context.stroke()
-    this.context.fill()
+    this.context.stroke();
+    this.context.fill();
     this.context.closePath();
   }
 }
