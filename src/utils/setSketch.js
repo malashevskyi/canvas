72f2e8edd7e31683fe8628b6a3f0b25818615a89
malashevskyi <shevsky1488@gmail.com>
2021-02-20 01:51:46 +0200

    fix canvas clearing

--- src/utils/setSketch.js
+++ src/utils/setSketch.js
@@ -5,9 +5,10 @@ import { store } from 'react-notifications-component';
 // import gsap from 'gsap';
 
 export default function setSketch(sketch, settings) {
-  return React.forwardRef((props, ref) => {    
-
+  return React.forwardRef((props, ref) => {
+    
     useEffect(() => {
+      const canvas = ref.current;
       // if (window.countRoute) {
       //   window.countRoute++
       // } else {
@@ -21,14 +22,16 @@ export default function setSketch(sketch, settings) {
 
       async function start() {
         manager = await canvasSketch(sketch, {
-          canvas: ref.current,
+          canvas,
           ...settings
         });
       };
       start()
         
       return () => {
-        // gsap.globalTimeline.pause();
+        // clear canvas before changing route
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
         
         // Unload previous canvas sketch
         manager.unload()
