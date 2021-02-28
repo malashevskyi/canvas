7c34b8a7a25a5a3ce45f93e4bf2a5161209d281e
malashevskyi <shevsky1488@gmail.com>
2021-02-28 17:36:22 +0200

    test AutoSizer

--- pages/index.js
+++ pages/index.js
@@ -16,28 +16,9 @@ const tls = [];
 
 
 const Index = ({ comments, postsData }) => {
-  // set initial height (for full hd resolution minus top bar and card margin);
-  // used to detect how many card render on server side;
-  const [height, setHeight] = useState(900);
-  // set initial width (for full hd resolution)
-  const [width, setWidth] = useState(1920);
-
-  // create own AutoSizer
-  const setDimensions = () => {
-    setHeight(window.innerHeight);
-    setWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    setDimensions();
-    window.addEventListener( 'resize', setDimensions);
-    return () => {
-      window.removeEventListener( 'resize', setDimensions);
-    }
-  }, []);
-
   const dataArr = [];
 
+  // create data arr from postsData
   Object.keys(postsData).forEach(key => {
     dataArr.push({
       id: key,
@@ -47,41 +28,40 @@ const Index = ({ comments, postsData }) => {
   });
 
   const Item = ({
-    cardHeight,
-    cardWidth,
-    columnCount,
-    gapSize,
-    rowCount,
     index,
+    postsData,
+    columnCount,
+    cardWidth,
+    cardHeight,
+    gapSize,
     style,
     key,
-    comments
   }) => {
     // This is the range of cards visible on this row, given the current width:
     const startIndex = index * columnCount;
     const stopIndex = Math.min(
-      comments.length - 1, // last item
+      postsData.length - 1, // last item
       startIndex + columnCount - 1
     );
     
     const cards = [];
-    // console.log(dataArr, 'dddddddddddd');
+
     for (let i = startIndex; i <= stopIndex; i++) {
-      const comment = comments[i];
-      // const imageSrc = `/images/previews/${comments[i].id}.png`;
-      // const title = comments[i].name;
-      // const id = comments[i].id;
+      const cardData = postsData[i];
+      const imageSrc = `/images/previews/${postsData[i].id}.png`;
+      const title = postsData[i].name;
+      const id = postsData[i].id;
   
-      // const link = (
-      //   <a>
-      //     <Image
-      //       src={imageSrc}
-      //       alt={title}
-      //       // width={290}
-      //       layout="fill"
-      //     />
-      //   </a>
-      // );
+      const link = (
+        <a>
+          <Image
+            src={imageSrc}
+            alt={title}
+            // width={290}
+            layout="fill"
+          />
+        </a>
+      );
   
       const card = (
         <div
@@ -93,31 +73,17 @@ const Index = ({ comments, postsData }) => {
             margin: `0 ${gapSize / 2}px`
           }}
         >
-          {/* {comment.title} */}
-          <br />
-          {comment.id}
-          {/* <Link href={`/post/${dataArr[i].id}`}>
+          <Link href={`/post/${dataArr[i].id}`}>
             {link}
-          </Link> */}
+          </Link>
         </div>
       );
       cards.push(card);
   
     }
-    // console.log(startIndex);
-    // console.log(stopIndex);
-    // console.log(cards);
-    // console.log(index);
-    
-    // return (
-    //   <div key={key} style={style}>
-    //     {comment.id}: {comment.name}
-    //   </div>
-    // )
   
     return (
       <div className="Item" style={style}>
-        {/* {comments} */}
         {cards}
       </div>
     );
@@ -125,35 +91,13 @@ const Index = ({ comments, postsData }) => {
 
 
 
-  function rowRenderer( comments, dataArr, rowCount, columnCount, { key, index, style }) {
-    // const dataArr = [];
-
-    // Object.keys(postsData).forEach(key => {
-    //   dataArr.push({
-    //     id: key,
-    //     number: postsData[key].number,
-    //     title: postsData[key].tags.join(' / ')
-    //   });
-    // });
-
-    // console.log(postsData[index]);
-    // return <div style={style} key={key}>lala</div>
-    // console.log(postsData[index]);
-    // const postData = postsData[index];
-    // const comment = comments[index];
-    // return (
-    //   <div key={key} style={style}>
-    //     {comment.id}: {comment.name}
-    //   </div>
-    // )
-
+  function rowRenderer( dataArr, columnCount, { key, index, style }) {
+    console.log('rowRenderer');
     return (
       <Item
         index={index}
-        columnCount={columnCount}
-        rowCount={rowCount}
         postsData={dataArr}
-        comments={comments}
+        columnCount={columnCount}
         cardWidth={290}
         cardHeight={CARD_HEIGHT}
         gapSize={GAP_SIZE}
@@ -163,40 +107,47 @@ const Index = ({ comments, postsData }) => {
     );
   }
 
-  // const columnCount = Math.floor( (width - GAP_SIZE) / (CARD_WIDTH + GAP_SIZE) );
-  // const rowCount = Math.ceil(dataArr.length / columnCount);
-  const columnCount = Math.floor( (width - GAP_SIZE) / (CARD_WIDTH + GAP_SIZE) );
-  const rowCount = Math.ceil(comments.length / columnCount);
-
-
   // issue https://github.com/bvaughn/react-virtualized/issues/851#issuecomment-338415738
-  // const ssrHeight = 1000;
-  // const ssrWidth = 1000;
+  const ssrHeight = 900; // set initial height for fullHD minus top bar and card margin
+  const ssrWidth = 500; // set initial width for fullHD 
 
   return (
     <div className="main">
-      {/* <AutoSizer defaultHeight={ssrHeight} defaultWidth={ssrWidth}>
-        {({ height, width }) => ( */}
-          <List
+      <AutoSizer>
+        {({ height, width }) => {
+          const columnCount = Math.floor( (width - GAP_SIZE) / (CARD_WIDTH + GAP_SIZE) );
+          const rowCount = Math.ceil(dataArr.length / columnCount);
+
+          console.log(`
+            width: ${width}
+            height: ${height}
+            rowCount: ${rowCount}
+          `);
+
+          return <List
             width={width}
             height={height}
             rowCount={rowCount}
             rowHeight={130}
-            rowRenderer={(rowArgs) => rowRenderer( comments, dataArr, rowCount, columnCount, rowArgs )}
-            overscanRowCount={0}
+            rowRenderer={(rowArgs) => rowRenderer( dataArr, columnCount, rowArgs )}
+            overscanRowCount={10}
           />
-        {/* )} */}
+        }}
+      </AutoSizer>
+      {/* <AutoSizer defaultHeight={ssrHeight} defaultWidth={ssrWidth}> */}
+        {/* {({ height, width }) => (
+        )} */}
       {/* </AutoSizer> */}
     </div>
   );
 };
 
 Index.getInitialProps = async function() {
-  const res = await fetch("http://jsonplaceholder.typicode.com/comments");
-  const data = await res.json();
+  // const res = await fetch("http://jsonplaceholder.typicode.com/comments");
+  // const data = await res.json();
 
   return {
-    comments: data,
+    // comments: data,
     postsData
   };
 }
