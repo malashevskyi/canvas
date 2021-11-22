a6de73f32af257d74eed56646ba9eeec2ec67b15
Vitalii Malashevskyi <malashevsky.v@gmail.com>
2021-11-22 17:38:13 +0200

    add show / clear utiles for canvas

--- utiles.js
+++ utiles.js
@@ -1,7 +1,23 @@
 export const clearCanvas = (type) => {
   const canvas = document.getElementById('canvas')
-  const context = canvas.getContext(type)
+  const canvasGL = document.getElementById('canvasGL')
 
-  context.clearRect(0, 0, window.innerWidth, window.innerHeight)
-  context.globalAlpha = 1
+  // context.clearRect(0, 0, window.innerWidth, window.innerHeight)
+  // context.globalAlpha = 1
+
+  if (type === 'webgl') {
+    canvasGL.style.display = 'none'
+  } else {
+    canvas.style.display = 'none'
+  }
+}
+export const showCanvas = (type) => {
+  const canvas = document.getElementById('canvas')
+  const canvasGL = document.getElementById('canvasGL')
+
+  if (type === 'webgl') {
+    canvasGL.style.display = 'block'
+  } else {
+    canvas.style.display = 'block'
+  }
 }
