3c6277921fa45374d5b17ab69e28541793d9881e
Vitalii Malashevskyi <malashevsky.v@gmail.com>
2021-12-26 18:40:39 +0200

    chore: add glsl ts support

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
       exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
+      use: ['ts-shader-loader'],
     })
     return config
   },
