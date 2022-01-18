bba94cb1a8151293e5b7562b7ed245328260b4d1
malashevskyi <shevsky1488@gmail.com>
2022-01-18 21:34:06 +0200

    fix: title in document warning

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>Canvas animations</title>
           <meta name="keywords" content="Canvas, animation" />
           <meta name="description" content="Canvas animations" />
           <meta property="og:title" content="Canvas animations" />
